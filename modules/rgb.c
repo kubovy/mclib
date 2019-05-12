eb8e1b480e2c991e4960c476d9c26e570c962e75
Jan Kubovy <jan.kubovy@bmw.de>
2019-05-12 21:43:00 +0200

    Refactor RGB module.

--- modules/rgb.c
+++ modules/rgb.c
@@ -8,85 +8,76 @@
 
 #ifdef RGB_ENABLED
 
-uint8_t RGB_redValue;
-uint8_t RGB_greenValue;
-uint8_t RGB_blueValue;
-uint8_t RGB_redTemp;
-uint8_t RGB_greenTemp;
-uint8_t RGB_blueTemp;
-uint8_t RGB_pattern;
-uint8_t RGB_delay;
-uint8_t RGB_min;
-uint8_t RGB_max;
-uint8_t RGB_count;
-
+#ifdef RGB_TIMER_PERIOD
 uint16_t RGB_counter = 0;
 
-void RGB_show(void) {
-    RGB_R_DUTY_CYCLE = RGB_redTemp;
-    RGB_G_DUTY_CYCLE = RGB_greenTemp;
-    RGB_B_DUTY_CYCLE = RGB_blueTemp;
-}
-
-#ifdef RGB_TIMER_PERIOD
 void RGB_update(void) {
     uint8_t percent;
-    if (RGB_count > 0) switch (RGB_pattern) {
+    if (RGB.count > 0) switch (RGB.pattern) {
         case RGB_PATTERN_LIGHT:
             percent = 255;
-            if (RGB_count < 0xFF) RGB_count--;
+            if (RGB.count < 0xFF) RGB.count--;
             break;
         case RGB_PATTERN_BLINK:
-            percent = (RGB_counter / RGB_delay) % 2 ? RGB_max : RGB_min;
-            if (RGB_count < 0xFF && percent == 0) RGB_count--;
+            percent = (RGB_counter / RGB.delay) % 2 ? RGB.max : RGB.min;
+            if (RGB.count < 0xFF && percent == 0) RGB.count--;
             break;
         case RGB_PATTERN_FADE_IN:
-            percent = (RGB_counter / RGB_delay) % 100;
-            if (RGB_count < 0xFF && percent == 99) RGB_count--;
-            percent = ((uint16_t) (RGB_max - RGB_min) * percent) / 99  + RGB_min;
+            percent = (RGB_counter / RGB.delay) % 100;
+            if (RGB.count < 0xFF && percent == 99) RGB.count--;
+            percent = ((uint16_t) (RGB.max - RGB.min) * percent) / 99  + RGB.min;
             break;
         case RGB_PATTERN_FADE_OUT:
-            percent = (RGB_counter / RGB_delay) % 100;
-            if (RGB_count < 0xFF && percent == 99) RGB_count--;
-            percent = RGB_max - ((uint16_t) (RGB_max - RGB_min) * percent) / 99;
+            percent = (RGB_counter / RGB.delay) % 100;
+            if (RGB.count < 0xFF && percent == 99) RGB.count--;
+            percent = RGB.max - ((uint16_t) (RGB.max - RGB.min) * percent) / 99;
             break;
         case RGB_PATTERN_FADE_TOGGLE:
-            percent = (RGB_counter / RGB_delay) % 200;
-            if (RGB_count < 0xFF && percent == 199) RGB_count--;
+            percent = (RGB_counter / RGB.delay) % 200;
+            if (RGB.count < 0xFF && percent == 199) RGB.count--;
             percent = percent > 100 ? 200 - percent : percent;
-            percent = (RGB_max - RGB_min) * percent / 100 + RGB_min;
+            percent = (RGB.max - RGB.min) * percent / 100 + RGB.min;
             break;
     }
-    RGB_redTemp = RGB_redValue * percent / 255;
-    RGB_greenTemp = RGB_greenValue * percent / 255;
-    RGB_blueTemp = RGB_blueValue * percent / 255;
-    RGB_show();
+#ifdef RGB_R_DUTY_CYCLE
+    RGB_R_DUTY_CYCLE = RGB.red * percent / RGB_DUTY_CYCLE_MAX;
+#endif
+#ifdef RGB_G_DUTY_CYCLE
+    RGB_G_DUTY_CYCLE = RGB.green * percent / RGB_DUTY_CYCLE_MAX;
+#endif
+#ifdef RGB_B_DUTY_CYCLE
+    RGB_B_DUTY_CYCLE = RGB.blue * percent / RGB_DUTY_CYCLE_MAX;
+#endif
     RGB_counter++;
 }
 #endif
 
-void RGB_off(void) {
+inline void RGB_off(void) {
     RGB_set(RGB_PATTERN_LIGHT, 0, 0, 0, 0, 0, 0, RGB_INDEFINED);
 }
 
 void RGB_set(uint8_t pattern, uint8_t red, uint8_t green, uint8_t blue, uint16_t delay, uint8_t min, uint8_t max, uint8_t count) {
-    RGB_pattern = pattern;
-    RGB_redValue = red;
-    RGB_greenValue = green;
-    RGB_blueValue = blue;
-    RGB_redTemp = red;
-    RGB_greenTemp = green;
-    RGB_blueTemp = blue;
 #ifdef RGB_TIMER_PERIOD
-    RGB_delay = delay / RGB_TIMER_PERIOD;
-#endif
-    RGB_min = min;
-    RGB_max = max;
-    RGB_count = count;
-#ifndef RGB_TIMER_PERIOD
-    RGB_show();
-#endif
+    RGB.red = red;
+    RGB.green = green;
+    RGB.blue = blue;
+    RGB.pattern = pattern;
+    RGB.delay = delay / RGB_TIMER_PERIOD;
+    RGB.min = min;
+    RGB.max = max;
+    RGB.count = count;
     RGB_counter = 0;
+#else
+#ifdef RGB_R_DUTY_CYCLE
+    RGB_R_DUTY_CYCLE = red * 255 / RGB_DUTY_CYCLE_MAX;
+#endif
+#ifdef RGB_G_DUTY_CYCLE
+    RGB_G_DUTY_CYCLE = green * 255 / RGB_DUTY_CYCLE_MAX;
+#endif
+#ifdef RGB_B_DUTY_CYCLE
+    RGB_B_DUTY_CYCLE = blue * 255 / RGB_DUTY_CYCLE_MAX;
+#endif
+#endif
 }
 
 #endif
--- modules/rgb.h
+++ modules/rgb.h
@@ -1,6 +1,17 @@
 /* 
  * File:   rgb.h
  * Author: Jan Kubovy &lt;jan@kubovy.eu&gt;
+ * 
+ * This module implement controlling and LED strip using PWM signal. It enables
+ * 3 color component RGB, but can be used also for single color LED strips by
+ * enable only one of those component or 2-3 single color LED strips by treating
+ * each color component as a separate strip.
+ * 
+ * Animations can be enabled by defining the RGB_TIMER_PERIOD constant.
+ * 
+ * By default the value of a duty cycle should range between 0 and 255. If the
+ * upper bound is different than the RGB_DUTY_CYCLE_MAX should be overwritten.
+ * Maximum upper bound is limited by 1 byte value (255).
  */
 #ifndef RGB_H
 #define	RGB_H
@@ -11,9 +22,7 @@ extern "C" {
 
 #include "../../config.h"
 
-#ifdef RGB_R_DUTY_CYCLE
-#ifdef RGB_G_DUTY_CYCLE
-#ifdef RGB_B_DUTY_CYCLE
+#if defined RGB_R_DUTY_CYCLE || defined RGB_G_DUTY_CYCLE || defined RGB_B_DUTY_CYCLE
 
 #define RGB_ENABLED
 #define RGB_INDEFINED 0xFF
@@ -37,6 +46,17 @@ typedef enum {
 } RGB_Pattern_t;
 
 #ifdef RGB_TIMER_PERIOD
+struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint8_t pattern;
+    uint8_t delay;
+    uint8_t min;
+    uint8_t max;
+    uint8_t count;
+} RGB;
+
 /**
  * Updates LED strip.
  * 
@@ -45,8 +65,10 @@ typedef enum {
 void RGB_update(void);
 #endif
 
-
-void RGB_off(void);
+/**
+ * Turns LED strip off.
+ */
+inline void RGB_off(void);
 
 /**
  * Set RGB LED strip configuration.
@@ -62,8 +84,6 @@ void RGB_off(void);
  */
 void RGB_set(uint8_t pattern, uint8_t red, uint8_t green, uint8_t blue, uint16_t delay, uint8_t min, uint8_t max, uint8_t count);
 
-#endif
-#endif
 #endif
 
 #ifdef	__cplusplus
